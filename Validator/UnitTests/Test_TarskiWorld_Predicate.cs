ba73fb9cff6cae37fb7fe151accc21a6cfbd6039
HanHan <hauffej@yahoo.de>
2020-03-05 22:57:04 +0100

    Fix Spaces

--- Validator/UnitTests/Test_TarskiWorld_Predicate.cs
+++ Validator/UnitTests/Test_TarskiWorld_Predicate.cs
@@ -39,6 +39,35 @@ namespace UnitTests
         }
 
         [TestMethod]
+        public void TarskiWorld_BackOf()
+        {
+            TarskiWorld world = new TarskiWorld();
+            List<string> sentences = new List<string>
+            {
+                "BackOf(a,b)",
+                "BackOf(a,c)",
+                "BackOf(a,d)",
+                "BackOf(a,e)"
+            };
+            List<WorldObject> worldObjects = new List<WorldObject>
+            {
+                new WorldObject(new List<string> { "a" }, new List<string> {TarskiWorldDataFields.TET, TarskiWorldDataFields.LARGE }, new List<object> {3, 3 }),
+                new WorldObject(new List<string> { "b" }, new List<string> {TarskiWorldDataFields.TET, TarskiWorldDataFields.MEDIUM }, new List<object> {4, 4 }),
+                new WorldObject(new List<string> { "c" }, new List<string> {TarskiWorldDataFields.TET, TarskiWorldDataFields.MEDIUM }, new List<object> {2, 2 }),
+                new WorldObject(new List<string> { "d" }, new List<string> {TarskiWorldDataFields.TET, TarskiWorldDataFields.MEDIUM }, new List<object> {3, 4 }),
+                new WorldObject(new List<string> { "e" }, new List<string> {TarskiWorldDataFields.TET, TarskiWorldDataFields.MEDIUM }, new List<object> {3, 2 }),
+            };
+            WorldParameter parameter = new WorldParameter(worldObjects, sentences);
+            var result = world.Check(parameter);
+
+            Assert.AreEqual(result.Result.Value.Count, 4);
+            Assert.IsFalse(result.Result.Value[0].Value);
+            Assert.IsTrue(result.Result.Value[1].Value);
+            Assert.IsFalse(result.Result.Value[2].Value);
+            Assert.IsTrue(result.Result.Value[3].Value);
+        }
+
+        [TestMethod]
         public void TarskiWorld_Between()
         {
             TarskiWorld world = new TarskiWorld();
@@ -68,15 +97,12 @@ namespace UnitTests
         }
 
         [TestMethod]
-        public void TarskiWorld_BackOf()
+        public void TarskiWorld_Larger()
         {
             TarskiWorld world = new TarskiWorld();
             List<string> sentences = new List<string>
             {
-                "BackOf(a,b)",
-                "BackOf(a,c)",
-                "BackOf(a,d)",
-                "BackOf(a,e)"
+                "Large(a) \u2227 Larger(a   ,b)"
             };
             List<WorldObject> worldObjects = new List<WorldObject>
             {
@@ -89,11 +115,8 @@ namespace UnitTests
             WorldParameter parameter = new WorldParameter(worldObjects, sentences);
             var result = world.Check(parameter);
 
-            Assert.AreEqual(result.Result.Value.Count, 4);
-            Assert.IsFalse(result.Result.Value[0].Value);
-            Assert.IsTrue(result.Result.Value[1].Value);
-            Assert.IsFalse(result.Result.Value[2].Value);
-            Assert.IsTrue(result.Result.Value[3].Value);
+            Assert.AreEqual(result.Result.Value.Count, 1);
+            Assert.IsTrue(result.Result.Value[0].Value);
         }
     }
 }
