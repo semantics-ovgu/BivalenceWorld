833a48ed906ad9611e53f975d417cf4fe274fd5e
HanHangit <hauffej@yahoo.de>
2020-07-29 20:29:39 +0200

    Fixing biconditional and improve parser (#45)

---
+++ Validator/UnitTests/Test_BivalenceWorld_Biconditional.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Validator;
+using Validator.World;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class Test_BivalenceWorld_Biconditional
+    {
+        [TestMethod]
+        public void BivalenceWorld_Biconditional()
+        {
+            BivalenceWorld world = new BivalenceWorld();
+            List<string> sentences = new List<string>
+            {
+                "Tet(a) \u2194 Tet(b)" ,
+                "(Tet(b) \u2227 Tet(c)) \u2194 (Tet(a) \u2227 Tet(c))",
+                "Tet(c) \u2194 Cube(a)",
+                "Tet(c) \u2194 Tet(a)"
+            };
+            List<WorldObject> worldObjects = new List<WorldObject>
+            {
+                new WorldObject(new List<string> { "a" }, new List<string> {BivalenceWorldDataFields.TET, BivalenceWorldDataFields.LARGE }, new List<object> {3, 3 }),
+                new WorldObject(new List<string> { "b" }, new List<string> {BivalenceWorldDataFields.TET, BivalenceWorldDataFields.MEDIUM }, new List<object> {4, 4 }),
+                new WorldObject(new List<string> { "c" }, new List<string> {BivalenceWorldDataFields.CUBE, BivalenceWorldDataFields.MEDIUM }, new List<object> {2, 2 })
+            };
+            WorldParameter parameter = new WorldParameter(worldObjects, sentences);
+            var result = world.Check(parameter);
+
+            Assert.AreEqual(result.Result.Value.Count, 4);
+            Assert.IsTrue(result.Result.Value[0].Value == EValidationResult.True);
+            Assert.IsTrue(result.Result.Value[1].Value == EValidationResult.True);
+            Assert.IsTrue(result.Result.Value[2].Value == EValidationResult.True);
+            Assert.IsFalse(result.Result.Value[3].Value == EValidationResult.True);
+        }
+    }
+}
--- Validator/Validator/Formula/Variable.cs
+++ Validator/Validator/Formula/Variable.cs
@@ -5,15 +5,19 @@ using System.Text;
 
 namespace Validator
 {
-    public class Constant : Argument
+    public class Variable : Argument
     {
-        public Constant(string name, string rawFormula) : base(new List<Argument>(), name, rawFormula)
+        public Variable(string name, string rawFormula) : base(new List<Argument>(), name, rawFormula)
         {
         }
 
         public override Result<string> GetPL1UniverseIdentifier(IWorldPL1Structure pL1Structure, Dictionary<string, string> dictVariables)
         {
-            if (pL1Structure.GetPl1Structure().GetConsts().ContainsKey(Name))
+            if (dictVariables.ContainsKey(Name))
+            {
+                return Result<string>.CreateResult(true, dictVariables[Name]);
+            }
+            else if (pL1Structure.GetPl1Structure().GetConsts().ContainsKey(Name))
             {
                 return Result<string>.CreateResult(true, Name);
             }
